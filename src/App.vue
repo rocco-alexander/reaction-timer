d520ef03eeacb861ee3af29e9d8838b713aa3620
rocco-alexander <roccoalexander@hotmail.com>
2022-09-21 21:43:13 -0400

    first

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ninja Reaction</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <ResultsVue v-if="showResults" :reactionTime="score"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Block from './components/Block.vue'
+import ResultsVue from './components/Results.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Block,
+    ResultsVue
+},
+  data(){
+    return{
+      isPlaying: false,
+      showResults: false,
+      delay: null,
+      score: null,
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 2000 + Math.random() * 5000 // 2000ms + random time between 0 and 5000ms
+      this.isPlaying = true;
+      this.showResults = false
+    },
+    endGame(reactionTime){
+    this.score = reactionTime
+    this.isPlaying = false
+    this.showResults = true
+    }
   }
 }
 </script>
@@ -20,7 +45,23 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 5px;
+  font-size: 1rem;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.3;
+  cursor: not-allowed;
+}
 </style>
+
---
+++ src/components/Results.vue
@@ -0,0 +1,32 @@
+<template>
+    <h3>Reaction Time: {{reactionTime}}ms</h3>
+    <h3>{{rank}}</h3>
+</template>
+
+<script>
+export default {
+    props:['reactionTime'],
+    data(){
+        return{
+            rank: null,
+
+        }
+    },
+    mounted(){ // update rank when mounted to the DOM
+        if(this.reactionTime < 250){
+            this.rank = 'Ninja Fingers'
+        }else if(this.reactionTime < 400){
+            this.rank= 'Rapid Reflexes'
+        }else{
+            this.rank="Snail Pace"
+        }
+    }
+}
+</script>
+
+<style>
+.rank{
+    color: #0faf87;
+    font-weight: bold;
+}
+</style>
